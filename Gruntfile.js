6a5a434919ae0fa59cf82d06df94ed34ed0328e9
Will Hastings <will@objectdotcreate.net>
2014-04-12 10:53:34 -0700

    Set up Grunt build script.

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    shell: {
+      test: {
+        command: 'npm test',
+        options: {
+          failOnError: true,
+          stderr: true,
+          stdout: true
+        }
+      }
+    },
+    uglify: {
+      build: {
+        files: {
+          'protomatter.min.js': ['protomatter.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-shell');
+
+  grunt.registerTask('build', ['shell:test', 'uglify:build']);
+};
