20105f55fefb9b04a7cfcd5b6fc741ca385b19e2
Will Hastings <will@objectdotcreate.net>
2014-04-12 16:04:14 -0700

    Add specs for Proto.create.

--- spec/protomatter.spec.js
+++ spec/protomatter.spec.js
@@ -38,4 +38,35 @@ describe('Protomatter', function() {
     });
   });
 
+  describe('Proto.create', function() {
+    var newObject,
+        Proto;
+
+    beforeEach(function() {
+      Proto = Protomatter.create({
+        initialize: function(var1, var2, var3) {
+          this.var1 = var1;
+          this.var2 = var2;
+          this.var3 = var3;
+        },
+        testProperty: 'test value'
+      });
+      newObject = Proto.create('value1', 'value2', 'value3');
+    });
+
+    it('returns an object with proto as its prototype', function() {
+      expect(Object.getPrototypeOf(newObject)).toBe(Proto);
+    });
+
+    it('returns an object that can access its properties', function() {
+      expect(newObject.testProperty).toBe('test value');
+    });
+
+    it('invokes its initialize function to set initial state', function() {
+      [1, 2, 3].forEach(function(num) {
+        expect(newObject['var' + num]).toBe('value' + num);
+      });
+    });
+  });
+
 });
